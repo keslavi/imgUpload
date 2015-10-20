2002aad778814955f491e4fe5c070aee55b9d62c
scranford <scranford@JAXLT0192.fl.int.nhsinc.net>
2015-10-20 18:05:27 -0400

    Initial Project

---
+++ src/web/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using web.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace web.Controllers
+{
+	[Route("/[controller]")]
+	public class HomeController : Controller
+    {
+        // GET: /<controller>/
+		[HttpGet]
+        public IActionResult Index()
+        {
+			var m =new Model1();
+            return View(m);
+        }
+    }
+}
---
+++ src/web/Controllers/WoundAssessmentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace web.Controllers
+{
+    [Route("api/[controller]")]
+    public class WoundAssessmentController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+	    [HttpPost]
+	    public Task<HttpResponse> Upload(int documentId)
+	    {
+			//still identifying the mvc5 way to do it, but can still probably do it old school
+			return null;
+	    }
+		
+    }
+}
